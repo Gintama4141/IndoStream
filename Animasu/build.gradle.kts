6925a07c34c13fafee2c75f03b891b7ae85a167d
Gintama4141 <106681440+Gintama4141@users.noreply.github.com>
2025-08-07 20:30:49 +0700

    Update build.gradle.kts

--- Animasu/build.gradle.kts
+++ Animasu/build.gradle.kts
@@ -1,26 +1,17 @@
 // use an integer for version numbers
-version = 13
+version = 13 // Sebaiknya naikkan versi ini ke 14 setelah perubahan
 
 cloudstream {
     language = "id"
-    // All of these properties are optional, you can safely remove them
-
-    // description = "Lorem Ipsum"
-     authors = listOf("Hexated", "TeKuma25")
-
-    /**
-     * Status int as the following:
-     * 0: Down
-     * 1: Ok
-     * 2: Slow
-     * 3: Beta only
-     * */
-    status = 1 // will be 3 if unspecified
+    authors = listOf("Hexated", "TeKuma25")
+    status = 1 
     tvTypes = listOf(
         "AnimeMovie",
         "OVA",
         "Anime",
     )
 
-    iconUrl = "https://www.google.com/s2/favicons?domain=animasu.cc&sz=%size%"
+    // ================== PERUBAHAN DI SINI ==================
+    iconUrl = "https://www.google.com/s2/favicons?domain=anime-indo.lol&sz=%size%"
+    // =======================================================
 }
